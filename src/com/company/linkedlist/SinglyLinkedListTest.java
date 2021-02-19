8904b0e70f4785a9e9491c8afb226bc51840b1c2
anyerin01 <anyerin01@gmail.com>
2021-02-19 00:40:23 -0500

    SinglyLinkedList

---
+++ src/com/company/linkedlist/SinglyLinkedListTest.java
@@ -0,0 +1,59 @@
+package com.company.linkedlist;
+
+/**
+ * linked list
+ * [][]-[][]-[][]합
+ * 노드에 다음 노드의 주소를 갖고 있는
+ * 길이가 정해져 있지 않은 데이터의 연결된 집
+ * 한 방향으로만 움직일 수 있는 것은 단방향 링크드 리스트
+ * 양쪽 방향으로 움직힐 수 있는 것은 양방향 링크드 리스트
+ *
+ */
+
+class Node{
+    int data;
+    Node next = null;
+    Node(int data){
+        this.data = data;
+    }
+
+    void append(int data){
+        Node end = new Node(data);
+        Node n = this;
+        while (n.next != null){
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+    void delete(int data){
+        Node n = this;
+        while (n.next != null){
+            if(n.next.data == data){
+                n.next = n.next.next;
+            }else
+                n = n.next;
+        }
+    }
+
+    void retrieve(){
+        Node n = this;
+        while (n.next != null){
+            System.out.print(n.data + " -> ");
+            n = n.next;
+        }
+        System.out.println(n.data);
+    }
+}
+
+public class SinglyLinkedListTest {
+    public static void main(String[] args){
+        Node head = new Node(1);
+        head.append(2);
+        head.append(3);
+        head.append(4);
+        head.retrieve();
+        head.delete(2);
+        head.retrieve();
+    }
+}
